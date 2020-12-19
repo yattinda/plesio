ae52a44bd75a52cada32ad17b51f9348eef6269f
TakahiroSono <taka.sono889@gmail.com>
2020-12-20 02:20:13 +0900

    add sidebar

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ import pandas as pd
 import requests
 import cv2
 
-# device = st.text_input("input your video/camera device", "0")
+# sidebar
+st.sidebar.markdown(
+  "# Paramator"
+)
+st.sidebar.select_slider("anger", options=list(range(101)))
+st.sidebar.select_slider("contempt", options=list(range(101)))
+st.sidebar.select_slider("disgust", options=list(range(101)))
+st.sidebar.select_slider("fear", options=list(range(101)))
+st.sidebar.select_slider("happiness", options=list(range(101)))
+st.sidebar.select_slider("neutral", options=list(range(101)))
+st.sidebar.select_slider("sadness", options=list(range(101)))
+st.sidebar.select_slider("surprise", options=list(range(101)))
 
 cap = cv2.VideoCapture(0)
 image_loc = st.empty()
@@ -11,7 +22,7 @@ image_loc = st.empty()
 while cap.isOpened():
     _, frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    image_loc.image(frame, width=960)
+    image_loc.image(frame, width=640)
     if cv2.waitKey() & 0xFF == ord("q"):
         break
 cap.release()
