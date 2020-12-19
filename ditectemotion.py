0f245a82955f0e0f7b99eaece8f6de284b181a67
yattinda <yattinda@gmail.com>
2020-12-20 05:34:40 +0900

    Create ditectmotion

--- ditectemotion.py
+++ ditectemotion.py
@@ -1,4 +1,4 @@
-def main():
+def ditectemotion(image_url):
     # <snippet_imports>
     import asyncio
     import io
@@ -18,14 +18,13 @@ def main():
     from msrest.authentication import CognitiveServicesCredentials
     from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
     # </snippet_imports>
-    
+
     #write KEY in seacret.py
     ENDPOINT = "https://yattinda.cognitiveservices.azure.com/"
-    IMAGE_BASE = "https://yattinda.blob.core.windows.net/test/test1.jpg"
 
     face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
-    single_face_image_url = 'https://publicdomainq.net/images/202012/16s/publicdomainq-0051317.jpg'
+    single_face_image_url = image_url
     single_image_name = os.path.basename(single_face_image_url)
 
     detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detectionModel='detection_02', return_face_attributes=["emotion"])
@@ -35,12 +34,12 @@ def main():
 
 
     for face in detected_faces:
-        print(face.face_attributes.emotion)
+        return face.face_attributes.emotion)
 
     #return detected_faces
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
         # response = requests.get(single_face_image_url)
         # img = Image.open(BytesIO(response.content))
         #
